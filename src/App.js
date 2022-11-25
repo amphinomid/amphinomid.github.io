ea4c89c6629084df77a3806f17b02257f01203b2
amphinomid <24998524+amphinomid@users.noreply.github.com>
2022-11-25 02:44:52 -0800

    setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import Doorway from './images/door.svg';
+import DoorwayOpen from './images/door_open.svg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div className="Content-outer">
+      <div className="Content-inner">
+        I'm a third-year Stanford undergrad studying CS and communication.
+        I help out with research in the <a href="https://hci.stanford.edu">HCI Group</a>{` `}
+        and write news for <a href="https://stanforddaily.com/author/anne24">The Daily</a>.
+        <br /><br />
+        In the past, I worked on coding projects for fun. I'm no longer maintaining them,
+        but their source codes are on <a href="https://github.com/amphinomid">Github</a>.{` `}
+        I also did two software engineering internships at Apple.
+        <br /><br />
+        <b>Random stuff</b><br />
+        There's a quote from Nuala O'Faolain that I love, that if "there were nothing else,
+        reading would--obviously--be worth living for."
+        <br /><br />
+        <a href="https://goodreads.com/amphinomid">
+          Reading
         </a>
-      </header>
+        <br /><br />
+        <a href="blog">
+          Musings that I have nowhere else to put
+        </a>
+        <br /><br />
+        An artifact from my foray into digital gardening:
+        <link rel="preload" href={DoorwayOpen} as="image" />
+        <div style={{ height: `10vh` }} />
+        <a href="https://amphinomid-garden.netlify.app" target="_blank" rel="noreferrer">
+          <div
+            style={{
+              display: `flex`,
+              flexDirection: `column`,
+              alignItems: `center`,
+              padding: `30px 200px 20px 100px`
+            }}
+          >
+            <span
+              className="doors"
+              aria-label="Closed double doors forming rounded top, framed by a leafy vine; upon hover, double doors open"
+            />
+          </div>
+        </a>
+      </div>
     </div>
   );
 }
---
+++ src/components/Blog.js
@@ -0,0 +1,19 @@
+import '../App.css';
+
+function Blog() {
+    return (
+        <div className="Content-outer">
+          <div className="Content-inner">
+                <b>On leave — 03/2022</b>
+                <br /><br />
+                I was on campus last fall, the first time since starting college. For a while I didn't know what to think or how to talk or act, because everything they say about learning to navigate Stanford is true. There are the things you're told, and there are the things no one tells you and you don't expect, or you do but you don't know what they mean until you've lived them. These you have to find out yourself and only then can you find out others feel the same way. This type of random discovery was relieving until it became perplexing.
+                <br /><br />
+                College meant incessant talk. As I talked more I learned the language of distance. I even worried I might sleepwalk into a chronic distance. "Keep in touch" is such a cheap phrase, I add "I mean it" when I really do.
+                <br /><br />
+                College could also be extraordinary. In winter I took a class--a general education requirement, at that--small enough to grow close. Squeezed onto a twin XL with a friend on a night I couldn't sleep in my own room. Watched a movie with two strangers, broadcast from a projector balanced atop a shelf. Biked back from McMurtry at night, screamed along to lyric videos of terrible songs, went on walks any time of day, all with people I care about. I can never tell if what I'm living is "the college experience," but I think it's close enough for now.
+            </div>
+        </div>
+    );
+}
+
+export default Blog;
